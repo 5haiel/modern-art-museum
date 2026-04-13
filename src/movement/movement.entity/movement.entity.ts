76cbecc8d28ea4ebd2edf1b5c120b85311d05857
Shaiel Jimenez <sm.jimenezp1@uniandes.edu.co>
2026-04-12 20:30:52 -0500

    Taller NESTJS persistencia

---
+++ src/movement/movement.entity/movement.entity.ts
@@ -0,0 +1,27 @@
+import { ArtistEntity } from 'src/artist/artist.entity/artist.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+
+@Entity()
+export class MovementEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  countryOfOrigin: string;
+
+  @ManyToMany(() => ArtistEntity, artist => artist.movements)
+  @JoinTable()
+  artists: ArtistEntity[];
+}
